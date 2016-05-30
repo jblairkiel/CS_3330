84d30ace052701f764d7e75dcb2b5156979f7ef6
jblairkiel <jblairkiel@gmail.com>
2016-05-30 07:43:43 -0500

    First Commit

---
+++ PS1/g.cpp
@@ -0,0 +1,41 @@
+/**
+ *	g.cpp - This progam implements the g() gunction 
+ *         as described in the handout.
+ *          *
+ *           * TODO: Include your name and course number here
+ *            
+ */
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int g(int n);
+
+int main(int argc, char **argv)
+{
+   cout << "g(-24) is " << g(-24) << endl;
+   cout << "g(-9) is " << g(-9) << endl;
+   cout << "g(-5) is " << g(-5) << endl;
+   cout << "g(0) is " << g(0) << endl;
+   cout << "g(6) is " << g(6) << endl;
+   cout << "g(25) is " << g(25) << endl;
+   cout << "g(32) is " << g(32) << endl;
+   cout << "g(47) is " << g(47) << endl;
+
+   cout << "\n** Press any key to continue **\n\n";
+   getchar();
+
+   return 0;
+}
+
+int g(int n)
+{
+   // TODO: Implement the details of the recursive g()
+   //    // function here based on the math definition in
+   //       // handout
+   //
+   return 0;
+}
