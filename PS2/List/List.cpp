ca5b4e6747e427fa349529edc276a347977583e2
theprocess <jblairkiel@gmail.com>
2016-06-07 20:50:34 -0500

    Beginning PS2

---
+++ PS2/List/List.cpp
@@ -0,0 +1,84 @@
+/**
+ * List.cpp - This file uses the C++ STL list API and implements a 
+ *            predicate function called isInList that will determine
+ *            if a given element is in a given list.
+ *
+ * TODO: Include your name and course number here.
+ */
+
+#include <iostream>
+#include <cstdio>
+#include <list>
+
+using namespace std;
+
+template <class Object>
+void printList(list<Object> myList);
+
+template <class Object>
+bool isInList(Object element, list<Object> myList);
+
+int main()
+{
+   list<int> myList;
+
+   myList.push_back(282);
+   myList.push_back(471);
+   myList.push_back(786);
+   myList.push_back(277);
+   myList.push_back(646);
+   myList.push_back(832);
+   myList.push_back(571);
+   myList.push_back(792);
+   myList.push_back(567);
+   myList.push_back(348);
+
+   cout << "myList:" << endl;
+   printList(myList);
+
+   cout << "172 is in list (0=false, 1=true): " << isInList(172, myList) << endl;
+   cout << "178 is in list (0=false, 1=true): " << isInList(178, myList) << endl;
+   cout << "282 is in list (0=false, 1=true): " << isInList(282, myList) << endl;
+   cout << "471 is in list (0=false, 1=true): " << isInList(471, myList) << endl;
+   cout << "571 is in list (0=false, 1=true): " << isInList(571, myList) << endl;
+   cout << "775 is in list (0=false, 1=true): " << isInList(775, myList) << endl;
+
+   cout << "\n** Press any key to continue **\n";
+   getchar();
+
+   return 0;
+} 
+
+template <class Object>
+void printList(list<Object> myList)
+{
+   // Use an iterator to walk the list. Print the value of 
+   // for each node.
+   typename list<Object>::iterator node;
+   for (node = myList.begin(); node != myList.end(); ++node)
+   {
+      // Get the contents of each node
+      Object value = (*node);   
+
+      // Print the value of the node
+      cout << " " << value; 
+   }
+
+   cout << endl << endl;
+
+   return;
+}
+
+template <class Object>
+bool isInList(Object element, list<Object> myList)
+{
+   bool found = false;
+
+   // Use an iterator to walk through the list: mylist
+   // as done in the printList() method. If value 
+   // equals element then return true or set the found 
+   // vairable equal to true and break out of the loop.
+
+   return found;
+}
+
---
+++ PS2/Stack/Stack.cpp
@@ -0,0 +1,76 @@
+/**
+ * Stack.cpp - This file uses the C++ STL stack API and implements 
+ *             a helper function called reverseStack that will 
+ *             reverse a given stack.
+ *
+ * TODO: Include your name and course number here.
+ */
+
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+template <class Object>
+void printStack(stack<Object> myStack);
+
+template <class Object>
+void reverseStack(stack<Object> &myStack);
+
+int main()
+{
+   stack<string> myStack;
+
+   myStack.push("423");
+   myStack.push("-");
+   myStack.push("462");
+   myStack.push("*");
+   myStack.push("457");
+   myStack.push("-");
+   myStack.push("259");
+   myStack.push("+");
+   myStack.push("346");
+   myStack.push("*");
+   myStack.push("582");
+
+   cout << "The contents of myStack:" << endl;
+   printStack(myStack);
+
+   cout << "The reverseStack of myStack:" << endl;
+   reverseStack(myStack);
+   printStack(myStack);
+
+   cout << "** Press any key to continue **";
+   getchar();
+
+   return 0;
+} 
+
+template <class Object>
+void printStack(stack<Object> myStack)
+{
+   while (!myStack.empty())
+   {
+      // Print the top item that is on the stack
+      cout << " " << myStack.top() << endl; 
+
+      // Pop the item off of the stack
+      myStack.pop();
+   }
+
+   cout << endl;
+
+   return;
+}
+
+template <class Object>
+void reverseStack(stack<Object> &myStack)
+{
+   // TODO: Implement the details for the reverseStack 
+   // function.
+
+   return;
+}
+
